bc549323401230638b8554a3df3a743cf0e22be9
Your Nameseyfullah <bayatseyfo@gmail.com>
2022-09-02 21:49:14 +0300

    admin oluşturuldu vd=17

--- website/settings.py
+++ website/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD':'2580',
+        'HOST':'localhost',
     }
 }
 
