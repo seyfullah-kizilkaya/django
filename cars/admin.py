ef2d1d7c83cdc7462964bd3529100776e113771e
Your Nameseyfullah <bayatseyfo@gmail.com>
2022-09-22 20:45:43 +0300

    araba verileri eklendi

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Car
+from django.utils.html import format_html
 # Register your models here.
-admin.site.register(Car)
+
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" />'.format(object.car_photo.url))
+
+    thumbnail.short_description = 'Car Image'
+    list_display = ('id','thumbnail', 'car_title', 'color', 'model', 'body_style', 'fuel_type', 'is_featured')
+    search_fields = ('id', 'car_title', 'color', 'model', 'body_style', 'fuel_type')
+    list_filter = ( 'color', 'model', 'fuel_type')
+admin.site.register(Car, CarAdmin)
