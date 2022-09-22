e123fdd2e1fee34a9b8e96de042ebfb634ea02ef
Your Nameseyfullah <bayatseyfo@gmail.com>
2022-09-22 22:20:15 +0300

    öene çıkan araba (hata)

--- cars/admin.py
+++ cars/admin.py
@@ -9,7 +9,10 @@ class CarAdmin(admin.ModelAdmin):
         return format_html('<img src="{}" width="40" />'.format(object.car_photo.url))
 
     thumbnail.short_description = 'Car Image'
-    list_display = ('id','thumbnail', 'car_title', 'color', 'model', 'body_style', 'fuel_type', 'is_featured')
-    search_fields = ('id', 'car_title', 'color', 'model', 'body_style', 'fuel_type')
+    list_display = ('id','thumbnail', 'car_title', 'city', 'color', 'model', 'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id','thumbnail', 'car_title')
+    search_fields = ('id', 'car_title', 'city', 'model', 'body_style', 'fuel_type')
     list_filter = ( 'color', 'model', 'fuel_type')
+    list_editable = ('is_featured',)
+
 admin.site.register(Car, CarAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Team
-
+from cars.models import Car
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
@@ -11,8 +11,10 @@ def home(request):
 
 def about(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     data = {
         'teams': teams,
+        'featured_cars': featured_cars,
     }
     return render(request, 'pages/about.html', data)
 def services(request):
